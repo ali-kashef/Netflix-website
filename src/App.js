eb394b0fd89021be7d977dba7460e2e1e7415fa4
ali kashef <ali.kashef28@gmail.com>
2025-02-04 13:20:46 +0330

    project was deployed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {HashRouter as Router, Route, Routes } from 'react-router-dom'
+import {Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import { CssBaseline } from '@mui/material'
 
@@ -20,6 +20,7 @@ export default function App() {
       
    
      <Navbar/>
+  
       <Routes>
        
         <Route exact path='/' element={<Home/>}/>
